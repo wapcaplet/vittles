06b8ae9c18b329f36c00ef4eaf48ad824306e4cd
Eric Pierce <wapcaplet88@gmail.com>
2011-05-16 13:07:46 -0600

    Added some inlines to forms

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from pyantry.cookbook.models import Preparation, Ingredient, Recipe
 
+class IngredientInline (admin.TabularInline):
+    """Displays ingredients in a table, with 10 rows.
+    """
+    model = Ingredient
+    extra = 10
+
+class RecipeAdmin (admin.ModelAdmin):
+    """Customized recipe admin interface, with ingredients included.
+    """
+    inlines = [IngredientInline]
+    list_display = ('name', 'servings', 'directions')
+
 admin.site.register(Preparation)
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
 
--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from pyantry.core.models import Category, Food, Unit, Equivalence
 
+# Inline forms
+
+class CategoryInline (admin.TabularInline):
+    model = Category
+    extra = 5
+    fk_name = 'parent'
+    verbose_name = 'Subcategory'
+    verbose_name_plural = 'Subcategories'
+
+class EquivalenceInline (admin.TabularInline):
+    model = Equivalence
+    extra = 5
+    fk_name = 'unit'
+
+class FoodInline (admin.TabularInline):
+    model = Food
+    extra = 5
+
+# Admin forms
+
 class CategoryAdmin (admin.ModelAdmin):
     list_display = ('name', 'parent')
     #list_editable = ('parent',)
     ordering = ('name',)
+    inlines = [FoodInline, CategoryInline]
 
 class FoodAdmin (admin.ModelAdmin):
     list_display = ('name', 'category')
@@ -17,6 +38,7 @@ class UnitAdmin (admin.ModelAdmin):
     list_display = ('name', 'abbr')
     list_editable = ('abbr',)
     ordering = ('name',)
+    inlines = [EquivalenceInline]
 
 class EquivalenceAdmin (admin.ModelAdmin):
     #list_display = ('__unicode__', 'unit', 'to_quantity', 'to_unit')
