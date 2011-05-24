963cf00a40baa74b1c1a76dd7ba7a3a5cbb7f89b
Eric Pierce <wapcaplet88@gmail.com>
2011-05-24 17:03:52 -0600

    Recipe view enhancements, static, templatetags

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Django settings for vittles project.
 
 import os
-VITTLES_ROOT = os.path.realpath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -71,6 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -105,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(VITTLES_ROOT, 'templates'),
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 # A sample logging configuration. The only tangible logging
