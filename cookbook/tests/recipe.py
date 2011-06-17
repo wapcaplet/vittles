80d1eafdda26b03857ff17850c295e232c030363
Eric Pierce <wapcaplet88@gmail.com>
2011-06-16 19:30:33 -0600

    More test cleanup and expansion

--- cookbook/tests/recipe.py
+++ cookbook/tests/recipe.py
@@ -11,42 +11,52 @@ class RecipeTest (TestCase):
         'test_nutrition_info',
         'test_recipe',
     ]
-
-    def test_recipe_nutrition_info_recalculation(self):
+    def setUp(self):
         # Foods
-        egg = Food.objects.get(name='egg')
-        flour = Food.objects.get(name='all-purpose flour')
-        butter = Food.objects.get(name='butter')
+        self.egg = Food.objects.get(name='egg')
+        self.flour = Food.objects.get(name='all-purpose flour')
+        self.butter = Food.objects.get(name='butter')
         # Units
-        cup = Unit.objects.get(name='cup')
-        ounce = Unit.objects.get(name='ounce')
+        self.cup = Unit.objects.get(name='cup')
+        self.ounce = Unit.objects.get(name='ounce')
         # Add an egg and 1 cup of flour to the pancakes recipe
-        pancakes = Recipe.objects.get(name='Pancakes')
-        pancakes.ingredients.create(quantity=1, food=egg)
-        pancakes.ingredients.create(quantity=1, unit=cup, food=flour)
-        pancakes.save()
+        self.pancakes = Recipe.objects.get(name='Pancakes')
+        self.pancakes.ingredients.create(quantity=1, food=self.egg)
+        self.pancakes.ingredients.create(quantity=1, unit=self.cup, food=self.flour)
+        self.pancakes.save()
 
-        egg_NI = FoodNutritionInfo.objects.get(food=egg, quantity=1, unit=None)
-        flour_NI = FoodNutritionInfo.objects.get(food=flour, quantity=1, unit=cup)
-        butter_NI = FoodNutritionInfo.objects.get(food=butter, quantity=1, unit=ounce)
+        self.egg_NI = FoodNutritionInfo.objects.get(
+            food=self.egg, quantity=1, unit=None)
+        self.flour_NI = FoodNutritionInfo.objects.get(
+            food=self.flour, quantity=1, unit=self.cup)
+        self.butter_NI = FoodNutritionInfo.objects.get(
+            food=self.butter, quantity=1, unit=self.ounce)
 
-        # Before recalculation
-        total_NI = egg_NI + flour_NI
-        self.assertTrue(pancakes.nutrition_info.is_equal(total_NI))
+
+    def test_recipe_nutrition_info_after_adding_ingredient(self):
+        # Before recalculation - only egg and flour
+        total_NI = self.egg_NI + self.flour_NI
+        self.assertTrue(self.pancakes.nutrition_info.is_equal(total_NI))
 
         # Add 1 oz. butter to recipe
-        pancakes.ingredients.create(quantity=1, unit=ounce, food=butter)
-        pancakes.save()
+        self.pancakes.ingredients.create(quantity=1, unit=self.ounce, food=self.butter)
+        self.pancakes.save()
 
         # Ensure that butter nutrition is now included in the total
-        total_NI = egg_NI + flour_NI + butter_NI
-        self.assertTrue(pancakes.nutrition_info.is_equal(total_NI))
+        total_NI = self.egg_NI + self.flour_NI + self.butter_NI
+        self.assertTrue(self.pancakes.nutrition_info.is_equal(total_NI))
 
-        # Make the recipe have two servings instead of one
-        pancakes.num_portions = 2.0
-        pancakes.save()
 
-        # Ensure the nutrition information is now halved
-        total_NI = (egg_NI + flour_NI + butter_NI) * 0.5
-        self.assertTrue(pancakes.nutrition_info.is_equal(total_NI))
+    def test_recipe_nutrition_info_after_changing_servings(self):
+        # If the recipe serves 2, nutrition information should be halved
+        self.pancakes.num_portions = 2.0
+        self.pancakes.save()
+        total_NI = (self.egg_NI + self.flour_NI) * 0.5
+        self.assertTrue(self.pancakes.nutrition_info.is_equal(total_NI))
+
+        # If the recipe serves 4, nutrition information should be quartered
+        self.pancakes.num_portions = 4.0
+        self.pancakes.save()
+        total_NI = (self.egg_NI + self.flour_NI) * 0.25
+        self.assertTrue(self.pancakes.nutrition_info.is_equal(total_NI))
 
--- cookbook/tests/view.py
+++ cookbook/tests/view.py
@@ -1,22 +1,38 @@
 from django.test import TestCase
-
 from django.test.client import Client
 from django.core.urlresolvers import reverse
+from cookbook.models import Recipe
 
 class CookbookIndexTest (TestCase):
+    fixtures = [
+        'test_food',
+        'test_unit',
+        'test_equivalence',
+        'test_food_nutrition_info',
+        'test_nutrition_info',
+        'test_recipe',
+        'test_recipe_nutrition_info',
+    ]
     def setUp(self):
         self.context = Client()
 
-    def test_cookbook_index(self):
+
+    def test_view_cookbook_index(self):
         index = self.context.get(reverse('cookbook-index'))
         self.assertEqual(index.status_code, 200)
+        # Index template is rendered
+        template_names = [t.name for t in index.templates]
+        self.assertTrue('cookbook/index.html' in template_names)
+        # One recipe category is shown
+        self.assertEqual(len(index.context['recipe_categories']), 1)
 
-        # Test templates rendered
-        #print('templates:')
-        #print([t.name for t in index.templates])
 
-        # Test view context
-        #print('context:')
-        #print(index.context['recipe_categories'])
+    def test_view_cookbook_recipe(self):
+        pancakes = Recipe.objects.get(name='Pancakes')
+        recipe = self.context.get(pancakes.get_absolute_url())
+        self.assertEqual(recipe.status_code, 200)
+        # Ensure context is correct
+        self.assertEqual(recipe.context['recipe'], pancakes)
+        self.assertEqual(recipe.context['nutrition_info'], pancakes.nutrition_info)
 
 
