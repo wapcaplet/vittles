7d060cb96f168f8b0e21e2761630e0ddee332ef6
Eric Pierce <wapcaplet88@gmail.com>
2011-06-21 16:49:45 -0600

    Remove superfluous error-checking
    Maybe should come back to this later, but right now the recalculate()
    method doesn't return an error status anymore.

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -50,15 +50,9 @@ class RecipeAdmin (admin.ModelAdmin):
         """Custom action to recalculate the `RecipeNutritionInfo`
         for the selected recipes.
         """
-        status = []
         for recipe in queryset:
-            status.append(recipe.nutrition_info.recalculate())
+            recipe.nutrition_info.recalculate()
             recipe.save()
-        message = str(status.count(True)) + \
-            " recipes with complete nutritional info; " + \
-            str(status.count(False)) + \
-            " recipes are missing some nutritional info."
-        self.message_user(request, message)
 
     refresh_nutrition_info.short_description = "Recalculate nutrition information"
 
