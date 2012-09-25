eee94b788df5051a46c39cc177657952ec2a4bf3
Eric Pierce <wapcaplet88@gmail.com>
2012-09-24 19:51:19 -0600

    Add a few more REST resources

--- vittles/rest/api.py
+++ vittles/rest/api.py
@@ -1,27 +1,50 @@
 from tastypie.resources import ModelResource
 from tastypie.api import Api
+from tastypie import fields
 from core.models import Food, Unit
-from cookbook.models import Recipe
+from cookbook.models import Recipe, Portion, RecipeCategory, Ingredient
+from tastypie.constants import ALL
+
+# Core
 
 class FoodResource (ModelResource):
     class Meta:
         queryset = Food.objects.all()
-        resource_name = 'food'
+        filtering = {
+            'name': ALL,
+        }
 
 class UnitResource (ModelResource):
     class Meta:
         queryset = Unit.objects.all()
-        resource_name = 'unit'
+
+class PortionResource (ModelResource):
+    class Meta:
+        queryset = Portion.objects.all()
+
+class RecipeCategoryResource (ModelResource):
+    class Meta:
+        queryset = RecipeCategory.objects.all()
+
+class IngredientResource (ModelResource):
+    class Meta:
+        queryset = Ingredient.objects.all()
 
 class RecipeResource (ModelResource):
+    portion = fields.ForeignKey(PortionResource, 'portion')
+    category = fields.ForeignKey(RecipeCategoryResource, 'category')
+    ingredients = fields.ToManyField('rest.api.IngredientResource', 'ingredients')
+
     class Meta:
         queryset = Recipe.objects.all()
-        resource_name = 'recipe'
 
 core_api = Api(api_name='core')
 core_api.register(FoodResource())
 core_api.register(UnitResource())
 
 cookbook_api = Api(api_name='cookbook')
+cookbook_api.register(PortionResource())
 cookbook_api.register(RecipeResource())
+cookbook_api.register(RecipeCategoryResource())
+cookbook_api.register(IngredientResource())
 
